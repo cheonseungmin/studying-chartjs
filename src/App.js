d3c2a710aff0fbf3284fd790a9b3c33601ab214d
Cheon seungmin <dudghsx12@gmail.com>
2022-07-17 16:32:57 +0900

    feat: 라인 차트와 addData 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 function App() {
+  const chartRef = useRef();
+  const [data, setData] = useState([]);
+
+  const chartData = {
+    datasets: [
+      {
+        label: 'count',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: false,
+  };
+
+  const addData = () => {
+    setData([
+      ...data,
+      {
+        x: (new Date()).toString().slice(16, 24),
+        y: Math.floor(Math.random() * 100),
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Line
+        ref={chartRef}
+        data={chartData}
+        options={options}
+        width={500}
+        height={500}
+      />
+      <button
+        type="button"
+        onClick={addData}
+      >
+        addData
+      </button>
+    </>
   );
 }
 
